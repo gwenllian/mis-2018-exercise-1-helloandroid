1e1faabe5bceb14edab5e76e3989b5dc8906469f
Pia <pia.fichtl@uni-weimar.de>
2018-04-13 00:19:30 +0200

    started exercise 1

--- app/src/main/java/com/example/mis/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/mis/helloandroid/MainActivity.java
@@ -4,7 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +26,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void helloWorld(View view) {
-        Toast toast = Toast.makeText(MainActivity.this, "hey there", Toast.LENGTH_SHORT);
+        EditText e = (EditText)findViewById(R.id.text);
+        String s = e.getText().toString();
+        String result = "";
+        try {
+            URL url = new URL(s);
+
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Scanner scan = new Scanner(in).useDelimiter("\\A");
+            result = scan.hasNext() ? scan.next() : "";
+
+        } finally {
+            urlConnection.disconnect();
+        }
+        }
+        catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+
+        Toast toast = Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0 ,0);
         toast.show();
     }
